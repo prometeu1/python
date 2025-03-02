50c85a3f2886e4fbafa48b3f22a20a0bbf8e4307
prometeu1 <kevin.bajurean@oteria.fr>
2025-03-02 15:53:37 +0100

    ajout d'un score

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
 import time
 
 words = ["estérifia", "dégrafasses", "sourissiez", "écroueras", "exprès", "reportera", "déchargerais", "galipotas", "débarbouillages", "agitez"]
+score = 0
 
 start_time = time.time()
 
@@ -8,11 +9,13 @@ for word in words:
     while True:
         user_input = input(f"Type '{word}': ")
         if user_input == word:
+            score += 1
             break
         else:
             print("Perdu, réessayez.")
+            score -= 1
 
 end_time = time.time()
 total_time = end_time - start_time
 
-print(f"Vous avez tapé {len(words)} mots en {total_time:.2f}s")
+print(f"Vous avez tapé {len(words)} mots en {total_time:.2f}s avec un score de {score} points.")
