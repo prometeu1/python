805894838d09ed9eba259ebc05b2a3361d6dc1d7
prometeu1 <kevin.bajurean@oteria.fr>
2025-03-02 15:11:30 +0100

    affichage de mot + test de frappe

--- python.py
+++ python.py
@@ -1,1 +1,4 @@
-print("hello")
+# je suis sur windows donc je vais pour l'instant tester de cette manière sans utiliser de dictionnaire de mots français 
+words = ["estérifia", "dégrafasses", "sourissiez", "écroueras", "exprès", "reportera", "déchargerais", "galipotas", "débarbouillages", "agitez"]
+
+input(f"Type '{word}': ")
