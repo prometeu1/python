5a2b92e5bf6a01b297b8ad169d4543433659353e
prometeu1 <kevin.bajurean@oteria.fr>
2025-03-02 15:27:44 +0100

    temps ajouté

--- python.py
+++ python.py
@@ -1,4 +1,14 @@
+import time
+
 # je suis sur windows donc je vais pour l'instant tester de cette manière sans utiliser de dictionnaire de mots français 
 words = ["estérifia", "dégrafasses", "sourissiez", "écroueras", "exprès", "reportera", "déchargerais", "galipotas", "débarbouillages", "agitez"]
 
-input(f"Type '{word}': ")
+start_time = time.time()
+
+for word in words:
+    input(f"Type '{word}': ")
+
+end_time = time.time()
+total_time = end_time - start_time
+
+print(f"Vous avez tapez {len(words)} mots en {total_time:.2f}s")
